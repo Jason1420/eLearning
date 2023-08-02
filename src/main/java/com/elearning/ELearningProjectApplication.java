48560c8ae69256f26a63de4401320d8263844ded
Jason1420 <hai.truong1420@gmail.com>
2023-08-02 16:28:57 +0700

    New Project

---
+++ src/main/java/com/elearning/ELearningProjectApplication.java
@@ -0,0 +1,13 @@
+package com.elearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ELearningProjectApplication.class, args);
+    }
+
+}
